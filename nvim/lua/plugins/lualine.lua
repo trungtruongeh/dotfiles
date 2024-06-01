053e256116c5e34fadd4c88551d91a88efe02921
Trung Truong <trung.truong@employmenthero.com>
2024-06-01 14:28:31 +0700

    update theme

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -10,7 +10,7 @@ return {
       require('lualine').setup {
         options = {
           icons_enabled = true,
-          theme = 'auto',
+          theme = vim.g.colors_name,
           component_separators = { left = '', right = '' },
           section_separators = { left = '', right = '' },
           disabled_filetypes = {
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -13,16 +13,67 @@ return {
       vim.g.everforest_better_performance = 1
       vim.cmd.colorscheme [[everforest]]
     end,
-    enabled = true,
+    enabled = false,
   },
   {
     'sainnhe/sonokai',
-    lazy = true,
+    lazy = false,
     config = function()
       vim.g.sonokai_style = "atlantis"
       vim.cmd.colorscheme [[sonokai]]
     end,
     enabled = false,
+  },
+  {
+    'maxmx03/dracula.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function ()
+      ---@type dracula
+      local dracula = require "dracula"
+
+      dracula.setup({
+      transparent = false,
+      on_colors = function (colors, color)
+        ---@type dracula.palette
+        return {
+          -- override or create new colors
+          mycolor = "#ffffff",
+        }
+      end,
+      on_highlights = function (colors, color)
+        ---@type dracula.highlights
+        return {
+          ---@type vim.api.keyset.highlight
+          Normal = { fg = colors.mycolor }
+        }
+      end,
+      plugins = {
+        ["nvim-treesitter"] = true,
+        ["nvim-lspconfig"] = true,
+        ["nvim-navic"] = true,
+        ["nvim-cmp"] = true,
+        ["indent-blankline.nvim"] = true,
+        ["neo-tree.nvim"] = true,
+        ["nvim-tree.lua"] = true,
+        ["which-key.nvim"] = true,
+        ["dashboard-nvim"] = true,
+        ["gitsigns.nvim"] = true,
+        ["neogit"] = true,
+        ["todo-comments.nvim"] = true,
+        ["lazy.nvim"] = true,
+        ["telescope.nvim"] = true,
+        ["noice.nvim"] = true,
+        ["hop.nvim"] = true,
+        ["mini.statusline"] = true,
+        ["mini.tabline"] = true,
+        ["mini.starter"] = true,
+        ["mini.cursorword"] = true,
+      }
+      })
+      vim.cmd.colorscheme 'dracula'
+      vim.cmd.colorscheme 'dracula-soft'
+    end
   },
   {
     'shaunsingh/nord.nvim',
